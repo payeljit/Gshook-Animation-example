6a7920f5483db1033a3803f4e3a0da5c13af6773
Payle <payelbanerjee9@yahoo.com>
2018-10-25 16:25:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ var leftImage = $('#left-img'),
     rightImage = $('#right-img'),
     logo = $('#logo'),
     btn = $('#btnPlay'),
+    listitems = $('ul li'),
     li1 = document.getElementsByClassName('lists')[0].firstElementChild,
     li2 = document.getElementsByClassName('lists2')[0].firstElementChild,
     tl = new TimelineLite();
@@ -16,6 +17,7 @@ var leftImage = $('#left-img'),
 tl
 .from(leftImage, 0.5,{ease: Back.easeOut.config(1.7), x: -500  })
 .from(rightImage, 0.5, {ease: SlowMo.ease.config(0.7, 0.7, false), y: -500 })
+.from(listitems, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeIn, delay: 0.8})
 .from(li1, 0.3, {y: -15, autoAlpha: 0, ease:Power1.easeIn, delay: 0.8})
 .from(li2, 0.3, {x: -15, autoAlpha: 0, ease:Power1.easeIn, delay: 1})
 .from(btn, 0.2, {ease: Bounce.easeIn, y: -500});
@@ -28,7 +30,7 @@ tl
 // TweenLite.from(lists, 1, { ease: Elastic.easeOut.config(1, 0.3), x: -500});
 
 TweenMax.from( logo, 0.5,
-      {css:{scale:0.05, opacity:0, rotation: 280},
+      {css:{scale:0.9, opacity:0, rotation: 280},
       ease:Quad.easeInOut
 });
 })(jQuery);
